6d72512f9e04a2b6ffef1a352b55e73cb44d3c33
Joseph Piñar <joseph.pinar.baltodano@est.una.ac.cr>
2024-08-31 13:46:01 -0600

    Conexión con la BD

--- database.py
+++ database.py
@@ -1,12 +1,21 @@
 import configparser
+import cx_Oracle
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 class Database:
+    """Class Database"""
     def __init__(self) -> None:
-        self.username = config['DATABASE']['USERNAME']
-        self.password = config['DATABASE']['PASSWORD']
-        self.dns = config['DATABASE']['DNS']
-        self.port = config['DATABASE']['PORT']
-        self.encoding = config['DATABASE']['ENCODING']
+        self._username = config['DATABASE']['USERNAME']
+        self._password = config['DATABASE']['PASSWORD']
+        self._url = config['DATABASE']['URL']
+        self._port = config['DATABASE']['PORT']
+        self._service_name = config['DATABASE']['SERVICE_NAME']
+        self._encoding = config['DATABASE']['ENCODING']
+        self.connection = any
+
+    def establecer_conexion(self):
+        """Método para iniciar la conexión con la BD"""
+        dns = cx_Oracle.makedns(self._url, self._port, self._service_name)
+        self.connection = cx_Oracle.connect(user=self._username, password=self._password, dns=dns, encoding=self._encoding)
